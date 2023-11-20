d14611bd09a7246e7e11d5ca23ae4d1d43f58890
Iamneo <techadmin@iamneo.ai>
2023-11-20 02:36:49 +0000

    Backup Commit

--- dotnetapp/Models/Player.cs
+++ dotnetapp/Models/Player.cs
@@ -1,4 +1,9 @@
-﻿namespace dotnetapp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace dotnetapp.Models
 {
-    
+    public class Plater
+    {
+        public int 
+    }
 }
