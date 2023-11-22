364af2dad6a161c6788398d4a1a9fe9371cc75ed
Iamneo <techadmin@iamneo.ai>
2023-11-22 16:17:14 +0000

    inc 3

--- angularapp/src/app/edit-team/edit-team.component.ts
+++ angularapp/src/app/edit-team/edit-team.component.ts
@@ -11,10 +11,7 @@ export class EditTeamComponent implements OnInit {
 id:number
 teamdata={id:0,name:''}
   constructor(private ps:AdminService,private ar:ActivatedRoute,private router:Router) {
-    const tid=this.ar.snapshot.paramMap.get('id')
-    this.id=Number(tid)
-    this.ps.editTeam(this.id).subscribe((data:any)=>{
-      this.playerdata=data
+ 
    }
 
   ngOnInit(): void {
